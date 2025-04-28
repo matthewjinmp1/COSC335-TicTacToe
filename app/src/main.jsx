838016d51ff3022dda081d7557bd5d27557855b4
kingepic84 <kingepic84@aol.com>
2025-04-28 12:32:26 -0400

    extra classes for styling

--- app/src/main.jsx
+++ app/src/main.jsx
@@ -12,187 +12,187 @@ import monument_image from "./monument.jpg"
 
 function Cottage() {
     return(
-        <div id="cottage" className="building_card">
-            <div className="building_name">
+        <div id="cottage" class="building_card">
+            <div class="building_name">
                 <div>Cottage</div>
-                <div className="cottage_piece"></div>
+                <div class="cottage_piece"></div>
             </div>
             <img src={cottage_image}></img> 
-            <div className="card_build">
-                <div className="pole"></div>
-                <div className="four">
-                    <div className="wheat" style={{gridArea: "2/3"}}></div>
-                    <div className="brick" style={{gridArea: "3/2"}}></div>
-                    <div className="glass" style={{gridArea: "3/3"}}></div>
+            <div class="card_build">
+                <div class="pole"></div>
+                <div class="four">
+                    <div class="wheat" style={{gridArea: "2/3"}}></div>
+                    <div class="brick" style={{gridArea: "3/2"}}></div>
+                    <div class="glass" style={{gridArea: "3/3"}}></div>
                 </div>
-                <div className="pole"></div>
+                <div class="pole"></div>
             </div>
-            <div className="building_bottom">3 points if this building is fed.</div>
+            <div class="building_bottom">3 points if this building is fed.</div>
         </div>
     );
 }
 
 function Chapel() {
     return(
-        <div id="chapel" className="building_card">
-            <div className="building_name">
+        <div id="chapel" class="building_card">
+            <div class="building_name">
                 <div>Chapel</div>
-                <div className="chapel_piece"></div>
+                <div class="chapel_piece"></div>
             </div>
             <img src={chapel_image}></img> 
-            <div className="card_build">
-                <div className="pole"></div>
-                <div className="six">
-                    <div className="glass" style={{gridArea: "2/4"}}></div>
-                    <div className="stone" style={{gridArea: "3/2"}}></div>
-                    <div className="glass" style={{gridArea: "3/3"}}></div>
-                    <div className="stone" style={{gridArea: "3/4"}}></div>
+            <div class="card_build">
+                <div class="pole"></div>
+                <div class="six">
+                    <div class="glass" style={{gridArea: "2/4"}}></div>
+                    <div class="stone" style={{gridArea: "3/2"}}></div>
+                    <div class="glass" style={{gridArea: "3/3"}}></div>
+                    <div class="stone" style={{gridArea: "3/4"}}></div>
                 </div>
-                <div className="pole"></div>
+                <div class="pole"></div>
             </div>
-            <div className="building_bottom">1 point for each fed cottage.</div>
+            <div class="building_bottom">1 point for each fed cottage.</div>
         </div>
     );
 }
 
 function Farm() {
     return(
-        <div id="farm" className="building_card">
-            <div className="building_name">
+        <div id="farm" class="building_card">
+            <div class="building_name">
                 <div>Farm</div>
-                <div className="farm_piece"></div>
+                <div class="farm_piece"></div>
             </div>
             <img src={farm_image}></img> 
-            <div className="card_build">
-                <div className="pole"></div>
-                <div className="four">
-                    <div className="wood" style={{gridArea: "2/2"}}></div>
-                    <div className="wood" style={{gridArea: "2/3"}}></div>
-                    <div className="wheat" style={{gridArea: "3/2"}}></div>
-                    <div className="wheat" style={{gridArea: "3/3"}}></div>
+            <div class="card_build">
+                <div class="pole"></div>
+                <div class="four">
+                    <div class="wood" style={{gridArea: "2/2"}}></div>
+                    <div class="wood" style={{gridArea: "2/3"}}></div>
+                    <div class="wheat" style={{gridArea: "3/2"}}></div>
+                    <div class="wheat" style={{gridArea: "3/3"}}></div>
                 </div>
-                <div className="pole"></div>
+                <div class="pole"></div>
             </div>
-            <div className="building_bottom">Feds 4 buildings anywhere in your town.</div>
+            <div class="building_bottom">Feds 4 buildings anywhere in your town.</div>
         </div>
     );
 }
 
 function Tavern() {
     return(
-        <div id="tavern" className="building_card">
-            <div className="building_name">
+        <div id="tavern" class="building_card">
+            <div class="building_name">
                 <div>Tavern</div>
-                <div className="tavern_piece"></div>
+                <div class="tavern_piece"></div>
             </div>
             <img src={tavern_image}></img> 
-            <div className="card_build">
-                <div className="pole"></div>
-                <div className="three">
-                    <div className="brick" style={{gridArea: "2/2"}}></div>
-                    <div className="brick" style={{gridArea: "2/3"}}></div>
-                    <div className="glass" style={{gridArea: "2/4"}}></div>
+            <div class="card_build">
+                <div class="pole"></div>
+                <div class="three">
+                    <div class="brick" style={{gridArea: "2/2"}}></div>
+                    <div class="brick" style={{gridArea: "2/3"}}></div>
+                    <div class="glass" style={{gridArea: "2/4"}}></div>
                 </div>
-                <div className="pole"></div>
+                <div class="pole"></div>
             </div>
-            <div className="building_bottom">Points based on your constructed taverns. 1/2/3/4/5 taverns = 2/5/8/13/20 points.</div>
+            <div class="building_bottom tavern_desc">Points based on your constructed taverns. <br></br> 1/2/3/4/5 taverns = 2/5/8/13/20 points.</div>
         </div>
     );
 }
 
 function Well() {
     return(
-        <div id="well" className="building_card">
-            <div className="building_name">
+        <div id="well" class="building_card">
+            <div class="building_name">
                 <div>Well</div>
-                <div className="well_piece"></div>
+                <div class="well_piece"></div>
             </div>
             <img src={well_image}></img> 
-            <div className="card_build">
-                <div className="pole"></div>
-                <div className="two">
-                    <div className="wood" style={{gridArea: "2/2"}}></div>
-                    <div className="stone" style={{gridArea: "2/3"}}></div>
+            <div class="card_build">
+                <div class="pole"></div>
+                <div class="two">
+                    <div class="wood" style={{gridArea: "2/2"}}></div>
+                    <div class="stone" style={{gridArea: "2/3"}}></div>
                 </div>
-                <div className="pole"></div>
+                <div class="pole"></div>
             </div>
-            <div className="building_bottom">1 point for each adjacent chapel.</div>
+            <div class="building_bottom">1 point for each adjacent chapel.</div>
         </div>
     );
 }
 
 function Theater() {
     return(
-        <div id="theater" className="building_card">
-            <div className="building_name">
+        <div id="theater" class="building_card">
+            <div class="building_name">
                 <div>Theater</div>
-                <div className="theater_piece"></div>
+                <div class="theater_piece"></div>
             </div>
             <img src={theater_image}></img> 
-            <div className="card_build">
-                <div className="pole"></div>
-                <div className="six">
-                    <div className="stone" style={{gridArea: "2/3"}}></div>
-                    <div className="wood" style={{gridArea: "3/2"}}></div>
-                    <div className="glass" style={{gridArea: "3/3"}}></div>
-                    <div className="wood" style={{gridArea: "3/4"}}></div>
+            <div class="card_build">
+                <div class="pole"></div>
+                <div class="six">
+                    <div class="stone" style={{gridArea: "2/3"}}></div>
+                    <div class="wood" style={{gridArea: "3/2"}}></div>
+                    <div class="glass" style={{gridArea: "3/3"}}></div>
+                    <div class="wood" style={{gridArea: "3/4"}}></div>
                 </div>
-                <div className="pole"></div>
+                <div class="pole"></div>
             </div>
-            <div className="building_bottom">1 point for each other unqiue building in the same row and column as this building.</div>
+            <div class="building_bottom theater_desc">1 point for each other unqiue building in the same row and column as this building.</div>
         </div>
     );
 }
 
 function Factory() {
     return(
-        <div id="factory" className="building_card">
-            <div className="building_name">
+        <div id="factory" class="building_card">
+            <div class="building_name">
                 <div>Factory</div>
-                <div className="factory_piece"></div>
+                <div class="factory_piece"></div>
             </div>
             <img src={factory_image}></img> 
-            <div className="card_build">
-                <div className="pole"></div>
-                <div className="eight">
-                    <div className="wood" style={{gridArea: "2/2"}}></div>
-                    <div className="brick" style={{gridArea: "3/2"}}></div>
-                    <div className="stone" style={{gridArea: "3/3"}}></div>
-                    <div className="stone" style={{gridArea: "3/4"}}></div>
-                    <div className="brick" style={{gridArea: "3/5"}}></div>
+            <div class="card_build">
+                <div class="pole"></div>
+                <div class="eight">
+                    <div class="wood" style={{gridArea: "2/2"}}></div>
+                    <div class="brick" style={{gridArea: "3/2"}}></div>
+                    <div class="stone" style={{gridArea: "3/3"}}></div>
+                    <div class="stone" style={{gridArea: "3/4"}}></div>
+                    <div class="brick" style={{gridArea: "3/5"}}></div>
                 </div>
-                <div className="pole"></div>
+                <div class="pole"></div>
             </div>
-            <div className="building_bottom factory_description">When constructed, place 1 of the 5 resources on this building. When this resources appears in the draw, you may choose any other resource.</div>
+            <div class="building_bottom factory_description">When constructed, place 1 of the 5 resources on this building. When this resources appears in the draw, you may choose any other resource.</div>
         </div>
     );
 }
 
 function Monument() {
     return(
-        <div id="monument" className="building_card">
-            <div className="building_name">
+        <div id="monument" class="building_card">
+            <div class="building_name">
                 <div>Cathedral of Caterina</div>
-                <div className="monument_piece"></div>
+                <div class="monument_piece"></div>
             </div>
             <img src={monument_image}></img> 
-            <div className="card_build">
-                <div className="pole"></div>
-                <div className="four">
-                    <div className="wheat" style={{gridArea: "2/3"}}></div>
-                    <div className="stone" style={{gridArea: "3/2"}}></div>
-                    <div className="glass" style={{gridArea: "3/3"}}></div>
+            <div class="card_build">
+                <div class="pole"></div>
+                <div class="four">
+                    <div class="wheat" style={{gridArea: "2/3"}}></div>
+                    <div class="stone" style={{gridArea: "3/2"}}></div>
+                    <div class="glass" style={{gridArea: "3/3"}}></div>
                 </div>
-                <div className="pole"></div>
+                <div class="pole"></div>
             </div>
-            <div className="building_bottom">2 points. Empty squares in your town are worth 0 points (instead of -1 point).</div>
+            <div class="building_bottom cathedral_desc">2 points. Empty squares in your town are worth 0 points (instead of -1 point).</div>
         </div>
     );
 }
 
 function Square({children, id}) {
     return(
-        <div className="square" id={id}>
+        <div class="square" id={id}>
             {children}
         </div>
     );
@@ -200,20 +200,20 @@ function Square({children, id}) {
 
 function ResourceCard({resource, index}) {
     return(
-        <div className="resource_card">
-            <div id={`resource_name${index}`} className="resource_card_name">{resource}</div>
-            <div className="resource_card_middle">
-                <div id={`resource_block${index}`} className={`center_piece ${resource}`}></div>
+        <div class="resource_card">
+            <div id={`resource_name${index}`} class="resource_card_name">{resource}</div>
+            <div class="resource_card_middle">
+                <div id={`resource_block${index}`} class={`center_piece ${resource}`}></div>
             </div>
-            <div className="resource_card_bottom"></div>
+            <div class="resource_card_bottom"></div>
         </div>
     );
 }
 
 function Game() {
     return(
-        <div className="main">
-            <div className="building_cards">
+        <div class="main">
+            <div class="building_cards">
                 <Cottage></Cottage>
                 <Chapel></Chapel>
                 <Farm></Farm>
@@ -222,22 +222,22 @@ function Game() {
                 <Theater></Theater>
                 <Factory></Factory>
             </div>
-            <div className="bottom">
-                <div className="resource_area">
-                    <div className="block_area">
-                        <div id="brick_block" className="brick" style={{gridArea: "2/2"}}></div>
-                        <div id="glass_block" className="glass" style={{gridArea: "2/4"}}></div>
-                        <div id="wood_block" className="wood" style={{gridArea: "2/6"}}></div>
-                        <div id="stone_block" className="stone" style={{gridArea: "2/8"}}></div>
-                        <div id="wheat_block" className="wheat" style={{gridArea: "2/10"}}></div>
+            <div class="bottom">
+                <div class="resource_area">
+                    <div class="block_area">
+                        <div id="brick_block" class="brick" style={{gridArea: "2/2"}}></div>
+                        <div id="glass_block" class="glass" style={{gridArea: "2/4"}}></div>
+                        <div id="wood_block" class="wood" style={{gridArea: "2/6"}}></div>
+                        <div id="stone_block" class="stone" style={{gridArea: "2/8"}}></div>
+                        <div id="wheat_block" class="wheat" style={{gridArea: "2/10"}}></div>
                     </div>
-                    <div className="resource_cards">
+                    <div class="resource_cards">
                         <ResourceCard resource={"Brick"} index={1}></ResourceCard>
                         <ResourceCard resource={"Glass"} index={2}></ResourceCard>
                         <ResourceCard resource={"Stone"} index={3}></ResourceCard>
                     </div>
                 </div>
-                <div id="board" className="board">
+                <div id="board" class="board">
                     <Square id="00"></Square>
                     <Square id="01"></Square>
                     <Square id="02"></Square>
@@ -255,14 +255,14 @@ function Game() {
                     <Square id="32"></Square>
                     <Square id="33"></Square>
                 </div>
-                <div className="right">
-                    <div className="monument_area">
+                <div class="right">
+                    <div class="monument_area">
                         <Monument></Monument>
                         <div></div>
                     </div>
-                    <div className="button_area">
-                        <div id="complete_town" className="complete_town">Complete Town</div>
-                        <div id="achievements" className="achievements">Achievements</div>
+                    <div class="button_area">
+                        <div id="complete_town" class="complete_town">Complete Town</div>
+                        <div id="achievements" class="achievements">Achievements</div>
                     </div>
                 </div>
             </div>
